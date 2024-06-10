863cbee692a77e8092d11e0dc94788dac8a3b0f5
Rob Woodward <rob@emailplus.org>
2024-06-10 16:58:39 +0100

    User new extension structure

--- src/SightlineApiBundle.php
+++ src/SightlineApiBundle.php
@@ -11,18 +11,66 @@
 
 namespace Robwdwd\SightlineApiBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 /**
  * Sightline API Symfony Bundle.
  */
-class SightlineApiBundle extends Bundle
+class SightlineApiBundle extends AbstractBundle
 {
-    /**
-     * Get path for bundle.
-     */
-    public function getPath(): string
+    public function configure(DefinitionConfigurator $definition): void
     {
-        return \dirname(__DIR__);
+        $definition->rootNode()
+            ->children()
+            ->scalarNode('hostname')
+            ->info('Hostname of the Sightline Leader.')
+            ->isRequired()
+            ->cannotBeEmpty()
+            ->end()
+            ->scalarNode('wskey')
+            ->info('Web services API key.')
+            ->isRequired()
+            ->cannotBeEmpty()
+            ->end()
+            ->scalarNode('resttoken')
+            ->info('REST API Token.')
+            ->isRequired()
+            ->cannotBeEmpty()
+            ->end()
+            ->scalarNode('username')
+            ->info('SOAP Username.')
+            ->isRequired()
+            ->cannotBeEmpty()
+            ->end()
+            ->scalarNode('password')
+            ->info('SOAP Password.')
+            ->isRequired()
+            ->cannotBeEmpty()
+            ->end()
+            ->scalarNode('wsdl')
+            ->info('WSDL description file for Sightline SOAP APIs.')
+            ->isRequired()
+            ->cannotBeEmpty()
+            ->end()
+            ->booleanNode('cache')
+            ->info('Turn caching on or off.')
+            ->defaultFalse()
+            ->end()
+            ->integerNode('cache_ttl')
+            ->info('Time to live of cached responses.')
+            ->defaultValue(900)
+            ->min(1)
+            ->end()
+            ->end()
+        ;
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        // load an XML, PHP or YAML file
+        $container->import('../config/services.xml');
     }
 }
